583e5bf92cf20560412b206062d26fd541253e22
Dimoshka <dmytro.prylutskyi@gmail.com>
2015-06-19 19:28:47 +0300

    Init

---
+++ Sources/ManagerReports/app/src/main/java/com/syject/managerreports/app/Application.java
@@ -0,0 +1,14 @@
+package com.syject.managerreports.app;
+
+import org.androidannotations.annotations.EApplication;
+
+@EApplication
+public class Application extends android.app.Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+
+}
