4021ebf2dff56425353bceef6f78990ab5cf90b0
Dimoshka <dmytro.prylutskyi@gmail.com>
2015-12-08 22:46:34 +0200

    create structure

--- Sources/ManagerReports/app/src/main/java/com/syject/managerreports/helper/DatabaseHelper.java
+++ Sources/ManagerReports/app/src/main/java/com/syject/managerreports/helper/DatabaseHelper.java
@@ -8,8 +8,16 @@ import android.util.Log;
 import com.j256.ormlite.android.apptools.OrmLiteSqliteOpenHelper;
 import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.TableUtils;
+import com.syject.managerreports.data.dao.MeetingDAO;
+import com.syject.managerreports.data.dao.PhoneDAO;
+import com.syject.managerreports.data.dao.ReportDAO;
+import com.syject.managerreports.data.dao.ReportHistoryDAO;
 import com.syject.managerreports.data.dao.UserDAO;
 import com.syject.managerreports.data.dao.UserGroupDAO;
+import com.syject.managerreports.data.db.Meeting;
+import com.syject.managerreports.data.db.Phone;
+import com.syject.managerreports.data.db.Report;
+import com.syject.managerreports.data.db.ReportHistory;
 import com.syject.managerreports.data.db.User;
 import com.syject.managerreports.data.db.UserGroup;
 
@@ -24,6 +32,10 @@ public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
     private static final int DATABASE_VERSION = 1;
 
     //ссылки на DAO соответсвующие сущностям, хранимым в БД
+    private MeetingDAO meetingDao = null;
+    private PhoneDAO phoneDao = null;
+    private ReportDAO reportDao = null;
+    private ReportHistoryDAO reportHistoryDao = null;
     private UserDAO userDao = null;
     private UserGroupDAO usergroupDao = null;
 
@@ -35,6 +47,10 @@ public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db, ConnectionSource connectionSource) {
         try {
+            TableUtils.createTable(connectionSource, Meeting.class);
+            TableUtils.createTable(connectionSource, Phone.class);
+            TableUtils.createTable(connectionSource, Report.class);
+            TableUtils.createTable(connectionSource, ReportHistory.class);
             TableUtils.createTable(connectionSource, UserGroup.class);
             TableUtils.createTable(connectionSource, User.class);
         } catch (SQLException e) {
@@ -49,6 +65,10 @@ public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase db, ConnectionSource connectionSource, int oldVer, int newVer) {
         try {
+            TableUtils.dropTable(connectionSource, Meeting.class, true);
+            TableUtils.dropTable(connectionSource, Phone.class, true);
+            TableUtils.dropTable(connectionSource, Report.class, true);
+            TableUtils.dropTable(connectionSource, ReportHistory.class, true);
             TableUtils.dropTable(connectionSource, UserGroup.class, true);
             TableUtils.dropTable(connectionSource, User.class, true);
             onCreate(db, connectionSource);
@@ -58,6 +78,34 @@ public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
         } catch (java.sql.SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    public MeetingDAO getMeetingDAO() throws SQLException, java.sql.SQLException {
+        if (meetingDao == null) {
+            meetingDao = new MeetingDAO(getConnectionSource(), Meeting.class);
+        }
+        return meetingDao;
+    }
+
+    public PhoneDAO getPhoneDAO() throws SQLException, java.sql.SQLException {
+        if (phoneDao == null) {
+            phoneDao = new PhoneDAO(getConnectionSource(), Phone.class);
+        }
+        return phoneDao;
+    }
+
+    public ReportDAO getReportDAO() throws SQLException, java.sql.SQLException {
+        if (reportDao == null) {
+            reportDao = new ReportDAO(getConnectionSource(), Report.class);
+        }
+        return reportDao;
+    }
+
+    public ReportHistoryDAO getReportHistoryDAO() throws SQLException, java.sql.SQLException {
+        if (reportHistoryDao == null) {
+            reportHistoryDao = new ReportHistoryDAO(getConnectionSource(), ReportHistory.class);
+        }
+        return reportHistoryDao;
     }
 
     public UserDAO getUserDAO() throws SQLException, java.sql.SQLException {
@@ -78,6 +126,11 @@ public class DatabaseHelper extends OrmLiteSqliteOpenHelper {
     @Override
     public void close() {
         super.close();
+        meetingDao = null;
+        phoneDao = null;
+        reportDao = null;
+        reportHistoryDao = null;
         userDao = null;
+        usergroupDao = null;
     }
 }
--- Sources/ManagerReports/app/src/main/java/com/syject/managerreports/ui/MainActivity.java
+++ Sources/ManagerReports/app/src/main/java/com/syject/managerreports/ui/MainActivity.java
@@ -1,5 +1,8 @@
 package com.syject.managerreports.ui;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -19,6 +22,8 @@ import java.sql.SQLException;
 @EActivity(R.layout.activity_main)
 @OptionsMenu(R.menu.menu_main)
 public class MainActivity extends BaseActivity {
+
+    private static final String Fragment_Tag = "CurrentHomeFragment";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,4 +49,21 @@ public class MainActivity extends BaseActivity {
     void settings_menuItem_click() {
 
     }
+
+    public void switchFragment(Fragment fragment) {
+        switchFragmentInternal(fragment, true);
+    }
+
+    private void switchFragmentInternal(Fragment fragment, boolean addToBackStack) {
+        FragmentManager supportFragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = supportFragmentManager.beginTransaction();
+
+        fragmentTransaction.replace(R.id.container, fragment, Fragment_Tag);
+
+        if(addToBackStack)
+            fragmentTransaction.addToBackStack(null);
+
+        fragmentTransaction.commit();
+    }
+
 }
